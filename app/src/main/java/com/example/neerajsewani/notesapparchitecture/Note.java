0bdfedd1a3f6cc8130163d8b9533a624fd60a2c2
Neeraj Sewani <neerajsewanisrt8@gmail.com>
2018-12-25 22:40:36 +0530

    Added androidx support.

--- app/src/main/java/com/example/neerajsewani/notesapparchitecture/Note.java
+++ app/src/main/java/com/example/neerajsewani/notesapparchitecture/Note.java
@@ -1,9 +1,7 @@
 package com.example.neerajsewani.notesapparchitecture;
 
-import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.PrimaryKey;
-import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.PrimaryKey;
+import androidx.room.PrimaryKey;
+import androidx.room.Entity;
 
 @Entity(tableName = "note_table")
 public class Note {
